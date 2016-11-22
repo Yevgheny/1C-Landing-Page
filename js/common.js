a65678c0d4684367b40118292d30fd59c28969b5
Yevgheny <yevgheny78@gmail.com>
2016-11-22 20:44:01 +0600

    header, main-screen, you-need sections have done

---
+++ js/common.js
@@ -0,0 +1,27 @@
+$(function() {
+	
+
+	// Smooth Scroll to block
+	$('a[href^="#l"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+        return false; 
+   });
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+$("#contact-call-back").validate();
+$("#contact-order-call").validate();
+$("#contact-get-price").validate();
+
